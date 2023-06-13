ac4c7c7aadcda1be26d93bb33c0bd3dab2cb904d
Laxman Dodda <38887341+Laxmanbalu@users.noreply.github.com>
2023-06-13 18:51:49 -0400

    Delete MyWeather directory

--- MyWeather/app/src/main/java/com/demo/openweatherapp/MyWeather.kt
+++
@@ -1,8 +0,0 @@
-package com.demo.openweatherapp
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class MyWeatherApplication : Application() {
-}
--- MyWeather/app/src/main/java/com/demo/openweatherapp/domain/repo/WeatherRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.demo.openweatherapp.domain.repo
-
-import com.demo.openweatherapp.domain.model.WeatherModel
-
-interface WeatherRepository {
-    suspend fun getWeatherData(cityName: String): WeatherModel
-}
